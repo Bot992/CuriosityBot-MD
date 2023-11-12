49f355f5f6698b3911c21dc0488400335fb73cd3
Diego Ofcc <147567344+Bot992@users.noreply.github.com>
2023-11-11 22:48:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,9 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['5214531287294', 'CuriosityBot-MD 🍧', true], ['5492266466080', 'Colaborado', true], ['5217294888993'], '5492266466080', '593968585383', '51992004117', '5492266613038', '573106040746', '50558124470', '51929972576' ]
+global.owner = [['573013114854', 'AmorBot-MD 🍧', true], ['573013114854',]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
 global.confirmCode = ''
 
 global.animxscans = ['400359205']
@@ -14,19 +12,19 @@ global.suittag = ['994400359205']
 global.mods = []
 global.prems = []
 
-global.packname = 'CuriosityBot-MD'
+global.packname = 'AmorBot-MD'
 global.author = ''
-global.wm = '© CuriosityBot-MD'
-global.wm2 = '𝗖𝗎𝗋𝗂𝗈𝗌𝗂𝗍𝗒 : 𝗕𝗈𝗍'
-global.azami = 'Azami'
-global.cb = 'CuriosityBot-MD'
+global.wm = '© AmorBot-MD'
+global.wm2 = 'Amor : 𝗕𝗈𝗍'
+global.azami = 'Diego'
+global.cb = 'AmorBot-MD'
 
 global.vs = '1.0.2'
 global.library = 'Baileys'
 global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = '5217294888993'
+global.dev = '573013114854'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
